3a2f00733db99daf9d3ddbc09a32834f88876e6e
R0DRlGUES <pedrohenrique.p.r@outlook.com>
2024-06-03 21:48:35 -0300

    Criando swagger

---
+++ src/main/java/br/unipar/FPJAuto/controller/ApiController.java
@@ -0,0 +1,15 @@
+package br.unipar.FPJAuto.controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+public class ApiController {
+
+    @GetMapping(path = "/api/hello")
+    public ResponseEntity<String> getAll() {
+        return ResponseEntity.ok("Hello World! I'm here");
+    }
+
+}
---
+++ src/main/java/br/unipar/FPJAuto/service/ProdutoService.java
@@ -0,0 +1,36 @@
+package br.unipar.FPJAuto.service;
+
+import br.unipar.FPJAuto.model.Produto;
+import br.unipar.FPJAuto.repository.ProdutoRepository;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProdutoService {
+
+    private final ProdutoRepository produtoRepository;
+
+    public ProdutoService(ProdutoRepository produtoRepository){
+        this.produtoRepository = produtoRepository;
+    }
+
+    public List<Produto> getAll(){
+        return this.produtoRepository.findAll();
+    }
+
+    public Produto save(Produto produto){
+        return this.produtoRepository.save(produto);
+    }
+
+    public Optional<Produto> getById(int id){
+        return this.produtoRepository.findById(id);
+    }
+
+    public void deleteById(int id){
+        this.produtoRepository.deleteById(id);
+    }
+
+}
